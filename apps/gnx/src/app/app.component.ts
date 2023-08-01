3200a5d0b3dc9f6d14e3f618ad576574e4b8ceeb
Carlos Sing <carlossing@gmail.com>
2023-08-01 11:37:15 -0500

    Initial commit

---
+++ apps/gnx/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gnx-prototipo-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'gnx';
+}

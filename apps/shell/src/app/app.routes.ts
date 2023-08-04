bdf2f9c2d6f9be993e2995da38293a02ed5b9c33
Carlos Sing <carlossing@gmail.com>
2023-08-04 01:13:58 -0500

    FIX

--- apps/shell/src/app/app.routes.ts
+++ apps/shell/src/app/app.routes.ts
@@ -2,14 +2,6 @@ import {Route} from '@angular/router';
 
 export const appRoutes: Route[] = [
   {
-    path: 'users',
-    loadChildren: () => import('users/Module').then((m) => m.RemoteEntryModule),
-  },
-  {
-    path: 'admin',
-    loadChildren: () => import('admin/Module').then((m) => m.RemoteEntryModule),
-  },
-  {
     path: 'auth',
     loadChildren: () => import('login/Module').then((m) => m.RemoteEntryModule),
   },
@@ -17,5 +9,13 @@ export const appRoutes: Route[] = [
     path: '',
     pathMatch: 'full',
     redirectTo: '/auth/login',
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('admin/Module').then((m) => m.RemoteEntryModule),
+  },
+  {
+    path: 'users',
+    loadChildren: () => import('users/Module').then((m) => m.RemoteEntryModule),
   },
 ];

4afed79dea5b9b5686e410cabe881f5d808497d2
Carlos Sing <carlossing@gmail.com>
2023-08-03 22:43:11 -0500

    FIX

--- apps/gnx/src/app/app.component.ts
+++ apps/gnx/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PrimeNGConfig} from 'primeng/api';
 
 @Component({
   selector: 'gnx-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'gnx';
+
+  constructor(private primengConfig: PrimeNGConfig) {
+  }
+
+  ngOnInit() {
+    this.primengConfig.ripple = true;
+  }
 }
